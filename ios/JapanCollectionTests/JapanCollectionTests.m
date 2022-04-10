5fdb87363c4463eed4f8e75e29a6c5fe5b5d629a
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2022-04-10 18:40:05 +0900

    successfully renamed

--- ios/JapanCollectionTests/JapanCollectionTests.m
+++ ios/JapanCollectionTests/JapanCollectionTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MuseumAppTests : XCTestCase
+@interface JapanCollectionTests : XCTestCase
 
 @end
 
-@implementation MuseumAppTests
+@implementation JapanCollectionTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
