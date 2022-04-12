09ebb671600a2c8d4fc7462f9cc1cd59fc6784a4
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2022-04-12 23:21:59 +0900

    add some ids

--- src/modules/exhibits/DAL/articleIds.ts
+++ src/modules/exhibits/DAL/articleIds.ts
@@ -2,4 +2,5 @@ export enum ARTICLE_IDS {
     GORO_NUDO_MASAMUNE = 'goro_nudo_masamune',
     TOKUGAWA = 'tokugawa',
     SAKAI = 'sakai',
+    HIKOSIRO_SADAMUNE = 'hikosiro_masamune',
 }
--- src/modules/exhibits/DAL/articleModalIds.ts
+++ src/modules/exhibits/DAL/articleModalIds.ts
@@ -3,4 +3,9 @@ export enum ARTICLE_MODAL_IDS {
     WAKIZASHI = 'wakizashi',
     SWORD_FORM = 'sword_form',
     SWORD_RATING_FUJISIRO = 'sword_rating_fujisiro',
+    SWORD_SMALLER = 'sword_smaller',
+    HONZE_MASAMUNE = 'honze_masamune',
+    DZUTETSU = 'dzutetsu',
+    DAISE = 'daise',
+    TANTO = 'tanto',
 }
