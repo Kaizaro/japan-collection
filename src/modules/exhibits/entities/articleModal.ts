8601a7332f98ea3962724f4d1261dab67e6bdfe4
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2022-04-13 21:24:11 +0900

    finish first part of articles

--- src/modules/exhibits/entities/articleModal.ts
+++ src/modules/exhibits/entities/articleModal.ts
@@ -1,6 +1,8 @@
 import {IExhibitLink} from '@src/modules/exhibits/entities/exhibitLink';
+import {ARTICLE_MODAL_IDS} from "@src/modules/exhibits/DAL/articleModalIds";
 
 export interface IArticleModal {
+    id: ARTICLE_MODAL_IDS;
     title: string;
     subtitle?: string;
     text: string;
