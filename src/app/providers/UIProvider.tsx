b1625b18f45ad35e2f7234e5f7cfc47c80eca56f
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2022-04-17 01:32:07 +0900

    add bottom-sheet provider

--- src/app/providers/UIProvider.tsx
+++ src/app/providers/UIProvider.tsx
@@ -4,6 +4,7 @@ import {StatusBar} from 'react-native';
 
 import {SafeAreaProvider} from 'react-native-safe-area-context/src/SafeAreaContext';
 import {NavigationContainer} from '@react-navigation/native';
+import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 
 import {APP_COLORS} from '@shared/config/colors';
 
@@ -15,9 +16,11 @@ const UIProvider: FC = ({children}) => {
                 barStyle={'dark-content'}
                 backgroundColor={APP_COLORS.GRAY_MAIN}
             />
-            <SafeAreaProvider>
-                <NavigationContainer>{children}</NavigationContainer>
-            </SafeAreaProvider>
+            <BottomSheetModalProvider>
+                <SafeAreaProvider>
+                    <NavigationContainer>{children}</NavigationContainer>
+                </SafeAreaProvider>
+            </BottomSheetModalProvider>
         </>
     );
 };
