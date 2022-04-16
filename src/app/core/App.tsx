41cfed2120036cfd72dda6dcc0f50afd58a0250c
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2022-04-17 01:48:54 +0900

    remove env deps

--- src/app/core/App.tsx
+++ src/app/core/App.tsx
@@ -1,6 +1,6 @@
 import React, {FC} from 'react';
 
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
+// import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 import {UIProvider} from '@app/providers';
 import {AppStack} from '@app/navigation/app-stack/AppStack';
