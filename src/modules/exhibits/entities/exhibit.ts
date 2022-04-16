98c535e649f4fdcc4d7c610340d999f357ad77ce
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2022-04-16 14:29:01 +0900

    made some updates for scheme

--- src/modules/exhibits/entities/exhibit.ts
+++ src/modules/exhibits/entities/exhibit.ts
@@ -10,7 +10,7 @@ export interface IExhibit {
     category: ExhibitCategory;
     subcategory: EXHIBIT_WEAPON_SUBCATEGORY;
     title: string;
-    subtitle?: string;
+    subtitle?: IExhibitLink;
     type: string;
     blacksmith_name: string;
     blacksmith_name_japanese: string;
@@ -19,6 +19,7 @@ export interface IExhibit {
     time: string;
     status: IExhibitStatus[];
     description: string;
-    links: IExhibitLink[];
+    linkWords?: IExhibitLink[];
+    dictionaryLinks?: IExhibitLink[];
     images?: ImageSourcePropType[];
 }
