4ef96253a293108dac6205ed930ef26cd7263f43
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2022-04-24 13:07:05 +0900

    made const for all tracks

--- src/modules/exhibits/DAL/exhibits/exhibitTracks.ts
+++ src/modules/exhibits/DAL/exhibits/exhibitTracks.ts
@@ -1,5 +1,410 @@
-
-
-export enum EXHIBIT_TRACKS {
-
-}
+export const EXHIBIT_TRACKS = {
+    1: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    2: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    3: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    4: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    5: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    6: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    7: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    8: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    9: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    10: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    11: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    12: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    13: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    14: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    15: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    16: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    17: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    18: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    19: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    20: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    21: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    22: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    23: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    24: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    25: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    26: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    27: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    28: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    29: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    30: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    31: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    32: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    33: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    34: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    35: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    36: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    37: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    38: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    39: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    40: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    41: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    42: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    43: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    44: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    45: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    46: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    47: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    48: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    49: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    50: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    51: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    52: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    53: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    54: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    55: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    56: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    57: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    58: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    59: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    60: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    61: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    62: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    63: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    64: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    65: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    66: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    67: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+    68: {
+        url: require('../../../../shared/assets/sound/exhibites/1.mp3'), // Load media from the app bundle
+        title: 'track_1',
+        artist: 'track_1',
+        duration: 30,
+    },
+};
