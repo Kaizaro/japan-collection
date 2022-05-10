0c77cd9c990e6eb912a7c8fcd35015b1c69c8188
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2022-05-10 19:22:57 +0900

    add all paths to images

--- src/modules/exhibits/DAL/exhibits/exhibitImages.ts
+++ src/modules/exhibits/DAL/exhibits/exhibitImages.ts
@@ -85,6 +85,85 @@ import i14_0 from '../../../../shared/assets/graphics/images/exhibits/13,14/0.pn
 import i13_0 from '../../../../shared/assets/graphics/images/exhibits/13,14/0.png';
 import i10_1 from '../../../../shared/assets/graphics/images/exhibits/10/1.png';
 import i10_0 from '../../../../shared/assets/graphics/images/exhibits/10/0.png';
+import i24_2 from '../../../../shared/assets/graphics/images/exhibits/24/2.png';
+import i24_1 from '../../../../shared/assets/graphics/images/exhibits/24/1.png';
+import i24_0 from '../../../../shared/assets/graphics/images/exhibits/24/0.png';
+import i25_2 from '../../../../shared/assets/graphics/images/exhibits/25/2.png';
+import i25_1 from '../../../../shared/assets/graphics/images/exhibits/25/1.png';
+import i25_0 from '../../../../shared/assets/graphics/images/exhibits/25/0.png';
+import i26_2 from '../../../../shared/assets/graphics/images/exhibits/26/2.png';
+import i26_1 from '../../../../shared/assets/graphics/images/exhibits/26/1.png';
+import i26_0 from '../../../../shared/assets/graphics/images/exhibits/26/0.png';
+import i42_2 from '../../../../shared/assets/graphics/images/exhibits/42/2.png';
+import i42_1 from '../../../../shared/assets/graphics/images/exhibits/42/1.png';
+import i42_0 from '../../../../shared/assets/graphics/images/exhibits/42/0.png';
+import i43_1 from '../../../../shared/assets/graphics/images/exhibits/43/1.png';
+import i43_0 from '../../../../shared/assets/graphics/images/exhibits/43/0.png';
+import i44_1 from '../../../../shared/assets/graphics/images/exhibits/44/1.png';
+import i44_0 from '../../../../shared/assets/graphics/images/exhibits/44/0.png';
+import i45_2 from '../../../../shared/assets/graphics/images/exhibits/45/2.png';
+import i45_1 from '../../../../shared/assets/graphics/images/exhibits/45/1.png';
+import i45_0 from '../../../../shared/assets/graphics/images/exhibits/45/0.png';
+import i46_1 from '../../../../shared/assets/graphics/images/exhibits/46/1.png';
+import i46_0 from '../../../../shared/assets/graphics/images/exhibits/46/0.png';
+import i47_1 from '../../../../shared/assets/graphics/images/exhibits/47/1.png';
+import i47_0 from '../../../../shared/assets/graphics/images/exhibits/47/0.png';
+import i48_2 from '../../../../shared/assets/graphics/images/exhibits/48/2.png';
+import i48_1 from '../../../../shared/assets/graphics/images/exhibits/48/1.png';
+import i48_0 from '../../../../shared/assets/graphics/images/exhibits/48/0.png';
+import i49_2 from '../../../../shared/assets/graphics/images/exhibits/49/2.png';
+import i49_1 from '../../../../shared/assets/graphics/images/exhibits/49/1.png';
+import i49_0 from '../../../../shared/assets/graphics/images/exhibits/49/0.png';
+import i50_2 from '../../../../shared/assets/graphics/images/exhibits/50/2.png';
+import i50_1 from '../../../../shared/assets/graphics/images/exhibits/50/1.png';
+import i50_0 from '../../../../shared/assets/graphics/images/exhibits/50/0.png';
+import i51_2 from '../../../../shared/assets/graphics/images/exhibits/51/2.png';
+import i51_1 from '../../../../shared/assets/graphics/images/exhibits/51/1.png';
+import i51_0 from '../../../../shared/assets/graphics/images/exhibits/51/0.png';
+import i52_2 from '../../../../shared/assets/graphics/images/exhibits/52/2.png';
+import i52_1 from '../../../../shared/assets/graphics/images/exhibits/52/1.png';
+import i52_0 from '../../../../shared/assets/graphics/images/exhibits/52/0.png';
+import i55_2 from '../../../../shared/assets/graphics/images/exhibits/55/2.png';
+import i55_1 from '../../../../shared/assets/graphics/images/exhibits/55/1.png';
+import i55_0 from '../../../../shared/assets/graphics/images/exhibits/55/0.png';
+import i56_2 from '../../../../shared/assets/graphics/images/exhibits/56/2.png';
+import i56_1 from '../../../../shared/assets/graphics/images/exhibits/56/1.png';
+import i56_0 from '../../../../shared/assets/graphics/images/exhibits/56/0.png';
+import i58_2 from '../../../../shared/assets/graphics/images/exhibits/58/2.png';
+import i58_1 from '../../../../shared/assets/graphics/images/exhibits/58/1.png';
+import i58_0 from '../../../../shared/assets/graphics/images/exhibits/58/0.png';
+import i59_2 from '../../../../shared/assets/graphics/images/exhibits/59/2.png';
+import i59_1 from '../../../../shared/assets/graphics/images/exhibits/59/1.png';
+import i59_0 from '../../../../shared/assets/graphics/images/exhibits/59/0.png';
+import i60_2 from '../../../../shared/assets/graphics/images/exhibits/60/2.png';
+import i60_1 from '../../../../shared/assets/graphics/images/exhibits/60/1.png';
+import i60_0 from '../../../../shared/assets/graphics/images/exhibits/60/0.png';
+import i61_2 from '../../../../shared/assets/graphics/images/exhibits/61/2.png';
+import i61_1 from '../../../../shared/assets/graphics/images/exhibits/61/1.png';
+import i61_0 from '../../../../shared/assets/graphics/images/exhibits/61/0.png';
+import i62_2 from '../../../../shared/assets/graphics/images/exhibits/62/2.png';
+import i62_1 from '../../../../shared/assets/graphics/images/exhibits/62/1.png';
+import i62_0 from '../../../../shared/assets/graphics/images/exhibits/62/0.png';
+import i63_2 from '../../../../shared/assets/graphics/images/exhibits/63/2.png';
+import i63_1 from '../../../../shared/assets/graphics/images/exhibits/63/1.png';
+import i63_0 from '../../../../shared/assets/graphics/images/exhibits/63/0.png';
+import i64_2 from '../../../../shared/assets/graphics/images/exhibits/64/2.png';
+import i64_1 from '../../../../shared/assets/graphics/images/exhibits/64/1.png';
+import i64_0 from '../../../../shared/assets/graphics/images/exhibits/64/0.png';
+import i65_2 from '../../../../shared/assets/graphics/images/exhibits/65/2.png';
+import i65_1 from '../../../../shared/assets/graphics/images/exhibits/65/1.png';
+import i65_0 from '../../../../shared/assets/graphics/images/exhibits/65/0.png';
+import i66_2 from '../../../../shared/assets/graphics/images/exhibits/66/2.png';
+import i66_1 from '../../../../shared/assets/graphics/images/exhibits/66/1.png';
+import i66_0 from '../../../../shared/assets/graphics/images/exhibits/66/0.png';
+import i67_2 from '../../../../shared/assets/graphics/images/exhibits/67/2.png';
+import i67_1 from '../../../../shared/assets/graphics/images/exhibits/67/1.png';
+import i67_0 from '../../../../shared/assets/graphics/images/exhibits/67/0.png';
+import i68_1 from '../../../../shared/assets/graphics/images/exhibits/68/1.png';
+import i68_0 from '../../../../shared/assets/graphics/images/exhibits/68/0.png';
+import i69_2 from '../../../../shared/assets/graphics/images/exhibits/69/2.png';
+import i69_1 from '../../../../shared/assets/graphics/images/exhibits/69/1.png';
+import i69_0 from '../../../../shared/assets/graphics/images/exhibits/69/0.png';
 
 export const EXHIBIT_IMAGES = {
     E2: {
@@ -136,6 +215,10 @@ export const EXHIBIT_IMAGES = {
         gallery: [i16_1, i16_2],
     },
     E22: [i22_1, i22_2, i22_3],
+    E23: {
+        card: i23_0,
+        gallery: [i16_1, i16_2],
+    },
     E27: [i27_1, i27_2, i27_3],
     E28: [i28_1, i28_2, i28_3],
     E29: [i29_1, i29_2, i29_3, i29_4, i29_5],
