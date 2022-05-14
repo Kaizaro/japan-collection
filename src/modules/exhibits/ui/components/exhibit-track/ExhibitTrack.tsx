a2e9c6072c4dbfb425b56b8b9094727d2706ac67
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2022-05-15 00:27:04 +0900

    made switch

--- src/modules/exhibits/ui/components/exhibit-track/ExhibitTrack.tsx
+++ src/modules/exhibits/ui/components/exhibit-track/ExhibitTrack.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useCallback, useEffect} from 'react';
+import React, {FC, useCallback, useEffect, useMemo, useState} from 'react';
 
 import {Alert, Image, View} from 'react-native';
 
@@ -9,6 +9,7 @@ import {IDefaultFCProps} from '@shared/types';
 
 import {ITrack} from '@src/shared/assets/sound/trackTypes';
 
+import playIcon from '../../../../../shared/assets/graphics/icons/player/play.png';
 import pauseIcon from '../../../../../shared/assets/graphics/icons/player/pause.png';
 import audioForwardIcon from '../../../../../shared/assets/graphics/icons/player/audioForward.png';
 import audioBackIcon from '../../../../../shared/assets/graphics/icons/player/audioBack.png';
@@ -29,8 +30,18 @@ const MOCK_TRACK = {
 };
 
 const ExhibitTrack: FC<IProps> = ({track}) => {
-    const getTrackState = useCallback(() => {
-        return TrackPlayer.getState();
+    const [playerStatus, setPlayerStatus] = useState<State>(State.None);
+    const isTrackPlaying = useMemo(
+        () =>
+            playerStatus === State.Playing || playerStatus === State.Buffering,
+        [playerStatus],
+    );
+
+    const getPlayerState = useCallback(async () => {
+        const playerState = await TrackPlayer.getState();
+        console.log(LOG_TAG, 'playerState', playerState);
+        setPlayerStatus(playerState);
+        return playerState;
     }, []);
 
     const getCurrentTrackPosition = useCallback(() => {
@@ -39,15 +50,18 @@ const ExhibitTrack: FC<IProps> = ({track}) => {
 
     const pauseTrack = useCallback(async () => {
         await TrackPlayer.pause();
-    }, []);
+        getPlayerState();
+    }, [getPlayerState]);
 
     const playTrack = useCallback(async () => {
         await TrackPlayer.play();
-    }, []);
+        getPlayerState();
+    }, [getPlayerState]);
 
     const stopTrack = useCallback(async () => {
         await TrackPlayer.stop();
-    }, []);
+        getPlayerState();
+    }, [getPlayerState]);
 
     const playForward = useCallback(async () => {
         const currentPosition = await getCurrentTrackPosition();
@@ -61,22 +75,51 @@ const ExhibitTrack: FC<IProps> = ({track}) => {
 
     const startTrack = useCallback(async () => {
         console.log(LOG_TAG, 'startTrackCallback', track);
-        const state = await getTrackState();
+        const state = await getPlayerState();
         console.log(LOG_TAG, state);
         if (state === State.Playing) {
             await stopTrack();
         }
         await TrackPlayer.add(track);
         await playTrack();
-    }, [getTrackState, playTrack, stopTrack, track]);
+    }, [getPlayerState, playTrack, stopTrack, track]);
 
     const resetTrack = useCallback(async () => {
         await TrackPlayer.reset();
         startTrack();
     }, [startTrack]);
 
+    const handlePausedPlayer = useCallback(async () => {
+        const trackDuration = await TrackPlayer.getDuration();
+        console.log(
+            LOG_TAG,
+            'trackDuration',
+            trackDuration,
+            trackDuration.toFixed(1),
+        );
+        const currentPosition = await TrackPlayer.getPosition();
+        console.log(
+            LOG_TAG,
+            'currentPosition',
+            currentPosition,
+            currentPosition.toFixed(1),
+        );
+        if (currentPosition.toFixed(1) === trackDuration.toFixed(1)) {
+            resetTrack();
+        } else {
+            await playTrack();
+        }
+    }, [playTrack, resetTrack]);
+
+    const handlePlayerError = useCallback(() => {
+        Alert.alert(
+            'Ошибка воспроизведения',
+            'Что-то пошло не так. Пожалуйста, перезагрузите приложение.',
+        );
+    }, []);
+
     const handlePlayPauseButtonPress = useCallback(async () => {
-        const state = await getTrackState();
+        const state = await getPlayerState();
         console.log(
             LOG_TAG,
             'handlePlayPauseButtonPressCallback',
@@ -87,39 +130,31 @@ const ExhibitTrack: FC<IProps> = ({track}) => {
         if (state === State.Playing) {
             await pauseTrack();
         } else if (state === State.Paused) {
-            const trackDuration = await TrackPlayer.getDuration();
-            console.log(
-                LOG_TAG,
-                'trackDuration',
-                trackDuration,
-                trackDuration.toFixed(1),
-            );
-            const currentPosition = await TrackPlayer.getPosition();
-            console.log(
-                LOG_TAG,
-                'currentPosition',
-                currentPosition,
-                currentPosition.toFixed(1),
-            );
-            if (currentPosition.toFixed(1) === trackDuration.toFixed(1)) {
-                resetTrack();
-            } else {
-                await playTrack();
-            }
+            handlePausedPlayer();
         } else {
             console.error(LOG_TAG, 'track state', state);
-            Alert.alert(
-                'Ошибка воспроизведения',
-                'Что-то пошло не так. Пожалуйста, перезагрузите приложение.',
-            );
+            handlePlayerError();
         }
-    }, [getTrackState, pauseTrack, playTrack, resetTrack]);
+    }, [getPlayerState, handlePausedPlayer, handlePlayerError, pauseTrack]);
 
     useEffect(() => {
         startTrack();
 
         return () => stopTrack();
     }, [startTrack, stopTrack]);
+
+    const PlayPauseButton = useMemo(
+        () => (
+            <PressableComponent onPress={handlePlayPauseButtonPress}>
+                <Image
+                    source={isTrackPlaying ? pauseIcon : playIcon}
+                    style={styles.trackPlayPauseIcon}
+                    resizeMode={'contain'}
+                />
+            </PressableComponent>
+        ),
+        [handlePlayPauseButtonPress, isTrackPlaying],
+    );
 
     return (
         <View style={styles.playerContainer}>
@@ -130,13 +165,7 @@ const ExhibitTrack: FC<IProps> = ({track}) => {
                     resizeMode={'contain'}
                 />
             </PressableComponent>
-            <PressableComponent onPress={handlePlayPauseButtonPress}>
-                <Image
-                    source={pauseIcon}
-                    style={styles.trackPlayPauseIcon}
-                    resizeMode={'contain'}
-                />
-            </PressableComponent>
+            {PlayPauseButton}
             <PressableComponent onPress={playForward}>
                 <Image
                     source={audioForwardIcon}
