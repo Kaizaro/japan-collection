255467143d73aa7bfc45cbbfcc0b74c38a0bb3cf
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2022-05-21 19:46:12 +0900

    made articles images

--- src/modules/exhibits/DAL/articles/articleImages.ts
+++ src/modules/exhibits/DAL/articles/articleImages.ts
@@ -1,5 +1,28 @@
-import goroNudoMasamune from '../../../../shared/assets/graphics/images/articles/goro-nudo-masamune.png';
+import sakaiClanHeraldic from '@shared/assets/graphics/images/articles/sakai-clan/sakai-clan-heraldic.jpg';
 
-export enum ARTICLE_IMAGES {
-    GORO_NUDO_MASAMUNE = goroNudoMasamune,
-}
+import tokugawaIeyasu from '../../../../shared/assets/graphics/images/articles/tokugawa-ieyasu/tokugawa-ieyasu.jpg';
+import tokugawaHeraldic from '../../../../shared/assets/graphics/images/articles/tokugawa-ieyasu/tokugawa-heraldic.jpg';
+import toetomiHideosi from '../../../../shared/assets/graphics/images/articles/toetomi-hideosi/toetomi-hideosi.jpg';
+import toetomiHeraldic from '../../../../shared/assets/graphics/images/articles/toetomi-hideosi/toetomi-heraldic.jpg';
+import takedaSingen from '../../../../shared/assets/graphics/images/articles/takeda-singen/takeda-singen.jpg';
+import takedaHeraldic from '../../../../shared/assets/graphics/images/articles/takeda-singen/takeda-heraldic.jpg';
+import sakaiTadatugu from '../../../../shared/assets/graphics/images/articles/sakai-clan/sakai-tadatugu.jpg';
+import odaNobunaga from '../../../../shared/assets/graphics/images/articles/oda-nobunaga/oda-nobunaga.jpg';
+import odaHeraldic from '../../../../shared/assets/graphics/images/articles/oda-nobunaga/oda-heraldic.jpg';
+import hondaTadakatsu from '../../../../shared/assets/graphics/images/articles/honda-tadakatsu/honda-tadakatsu.jpg';
+import hondaHeraldic from '../../../../shared/assets/graphics/images/articles/honda-tadakatsu/honda-heraldic.png';
+import hattoriHanzoHeraldic from '../../../../shared/assets/graphics/images/articles/hattori-hanzo/hattori-heraldic.jpg';
+import hattoriHanzo from '../../../../shared/assets/graphics/images/articles/hattori-hanzo/hattori-hanzo.jpg';
+import hattoriHanzoYari from '../../../../shared/assets/graphics/images/articles/hattori-hanzo/hattori-hanzo-yari.jpg';
+import goroNudoMasamune from '../../../../shared/assets/graphics/images/articles/goro-nudo-masamune/goro-nudo-masamune.png';
+
+export const ARTICLE_IMAGES = {
+    GORO_NUDO_MASAMUNE: [goroNudoMasamune],
+    HATORI_HANZO: [hattoriHanzo, hattoriHanzoYari, hattoriHanzoHeraldic],
+    HONDA_TADAKATSU: [hondaTadakatsu, hondaHeraldic],
+    ODA_NOBUNAGA: [odaNobunaga, odaHeraldic],
+    SAKAI_CLAN: [sakaiTadatugu, sakaiClanHeraldic],
+    TAKEDA_SINGEN: [takedaSingen, takedaHeraldic],
+    TOETOMI_HIDEOSI: [toetomiHideosi, toetomiHeraldic],
+    TOKUGAWA_IEYASU: [tokugawaIeyasu, tokugawaHeraldic],
+};
