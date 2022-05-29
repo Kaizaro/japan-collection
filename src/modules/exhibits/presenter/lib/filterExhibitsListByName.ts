5efb6d415dede6372df86b58741e9452209f3922
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2022-05-29 14:03:38 +0900

    remove console

--- src/modules/exhibits/presenter/lib/filterExhibitsListByName.ts
+++ src/modules/exhibits/presenter/lib/filterExhibitsListByName.ts
@@ -2,19 +2,14 @@ import {IExhibit} from '@src/modules/exhibits/entities';
 import {EXHIBITS} from '@src/modules/exhibits/DAL/exhibits/exhibits';
 
 export const filterExhibitsListByName = (searchText: string): IExhibit[] => {
-    console.log('@FILTER', searchText);
-    console.log('@FILTER', searchText.length);
     if (searchText.length === 0) {
         return EXHIBITS;
     } else {
         const parsedNumber = parseInt(searchText, 10);
-        console.log('@FILTER', parsedNumber);
-        console.log('@FILTER', 'isNaN', !isNaN(parsedNumber));
         if (!isNaN(parsedNumber)) {
             const filteredExhibits = EXHIBITS.filter((exhibit) =>
                 exhibit.exhibit_number?.toString().startsWith(searchText),
             );
-            console.log('@FILTER', 'filteredExhibits', filteredExhibits);
 
             if (filteredExhibits) {
                 return filteredExhibits;
@@ -25,7 +20,6 @@ export const filterExhibitsListByName = (searchText: string): IExhibit[] => {
             const filteredExhibits = EXHIBITS.filter((exhibit) =>
                 exhibit.title.startsWith(searchText),
             );
-            console.log('@FILTER', 'filteredExhibits', filteredExhibits);
 
             if (filteredExhibits) {
                 return filteredExhibits;
