61c910d51cc2771a99dc651896e49977111e5b84
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2022-05-29 14:03:03 +0900

    made search engine for strings too

--- src/modules/exhibits/presenter/lib/filterExhibitsListByName.ts
+++ src/modules/exhibits/presenter/lib/filterExhibitsListByName.ts
@@ -6,13 +6,24 @@ export const filterExhibitsListByName = (searchText: string): IExhibit[] => {
     console.log('@FILTER', searchText.length);
     if (searchText.length === 0) {
         return EXHIBITS;
-    } else if (searchText.length < 3) {
+    } else {
         const parsedNumber = parseInt(searchText, 10);
         console.log('@FILTER', parsedNumber);
         console.log('@FILTER', 'isNaN', !isNaN(parsedNumber));
         if (!isNaN(parsedNumber)) {
             const filteredExhibits = EXHIBITS.filter((exhibit) =>
                 exhibit.exhibit_number?.toString().startsWith(searchText),
+            );
+            console.log('@FILTER', 'filteredExhibits', filteredExhibits);
+
+            if (filteredExhibits) {
+                return filteredExhibits;
+            } else {
+                return [];
+            }
+        } else {
+            const filteredExhibits = EXHIBITS.filter((exhibit) =>
+                exhibit.title.startsWith(searchText),
             );
             console.log('@FILTER', 'filteredExhibits', filteredExhibits);
 
