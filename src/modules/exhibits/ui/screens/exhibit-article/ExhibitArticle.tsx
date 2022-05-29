d4d420d64d16cb027787a3432fe311985b47c30a
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2022-05-29 14:54:28 +0900

    made carousel for articles

--- src/modules/exhibits/ui/screens/exhibit-article/ExhibitArticle.tsx
+++ src/modules/exhibits/ui/screens/exhibit-article/ExhibitArticle.tsx
@@ -11,44 +11,53 @@ import {DEFAULT_BOTTOM_INSET} from '@shared/constants/styles';
 import {APP_TEXT_COLORS} from '@shared/config/colors';
 
 import {ExhibitDescription} from '@src/modules/exhibits/ui/components/exhibit-description';
+import {ExhibitCarousel} from '@src/modules/exhibits/ui/components/exhibit-carousel';
 import {IArticle} from '@src/modules/exhibits/entities/article';
 
 import {articleGoroNudoMasamuneStyles as styles} from './styles';
+import {PaddingContainer} from "@shared/ui/container/PaddingContainer";
 
 const ExhibitArticle: FC = () => {
     const {article} = useRoute().params as TRecord<IArticle>;
 
     return (
-        <ComponentContainer innerStyle={styles.container}>
+        <ComponentContainer
+            isHorizontalPaddingEnabled={false}
+            innerStyle={styles.container}>
             <ScrollView
                 showsVerticalScrollIndicator={false}
                 bounces={false}
                 contentInset={DEFAULT_BOTTOM_INSET}>
                 <View>
-                    {/* TODO need to fix images */}
-                    <Image
-                        source={article.images[0]}
-                        style={styles.image}
-                        resizeMode={'contain'}
-                    />
+                    <ExhibitCarousel images={article.images} />
                 </View>
-                <View style={styles.title}>
-                    <HeaderText fontSize={48}>{article.title}</HeaderText>
-                </View>
-                {article.subtitle && (
-                    <View>
-                        <RegularText
-                            fontSize={24}
-                            color={APP_TEXT_COLORS.SECONDARY}>
-                            {article.subtitle}
-                        </RegularText>
+                {/*<View>*/}
+                {/*    /!* TODO need to fix images *!/*/}
+                {/*    <Image*/}
+                {/*        source={article.images[0]}*/}
+                {/*        style={styles.image}*/}
+                {/*        resizeMode={'contain'}*/}
+                {/*    />*/}
+                {/*</View>*/}
+                <PaddingContainer>
+                    <View style={styles.title}>
+                        <HeaderText fontSize={48}>{article.title}</HeaderText>
                     </View>
-                )}
-                <ExhibitDescription
-                    description={article.description}
-                    linkWords={article.linkWords}
-                    innerStyle={styles.description}
-                />
+                    {article.subtitle && (
+                        <View>
+                            <RegularText
+                                fontSize={24}
+                                color={APP_TEXT_COLORS.SECONDARY}>
+                                {article.subtitle}
+                            </RegularText>
+                        </View>
+                    )}
+                    <ExhibitDescription
+                        description={article.description}
+                        linkWords={article.linkWords}
+                        innerStyle={styles.description}
+                    />
+                </PaddingContainer>
             </ScrollView>
         </ComponentContainer>
     );
