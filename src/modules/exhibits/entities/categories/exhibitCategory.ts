a651fc3e55ba46d9055724813a667b54e6face5a
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2022-06-05 11:31:14 +0900

    made categories

--- src/modules/exhibits/entities/categories/exhibitCategory.ts
+++ src/modules/exhibits/entities/categories/exhibitCategory.ts
@@ -1,13 +1,4 @@
 export enum EXHIBIT_CATEGORY {
     JAPANESE_WEAPON = 'JAPANESE_WEAPON',
     JAPANESE_CULTURE = 'JAPANESE_CULTURE',
-
-    // BLADE_WEAPON = 'blade_weapon',
-    // PERCUSSION_WEAPON = 'percussion_weapon',
-    // BLUNT_WEAPON = 'blunt_weapon',
-    // THROWABLE_WEAPON = 'throwable_weapon',
-    // FIREARMS = 'firearms',
-    // ARMOR = 'armor',
-    // TOOLS = 'tools',
-    // CULTURE = 'culture',
 }
